e94ab91bb7ae0f31b24f219a06b0d320bc6338be
YaroslavKabanov <ya.kabanov@gmail.com>
2016-02-03 14:34:35 -0500

    Added background scroll, plane can move, plane has borders

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+	// PUBLOC INSTANCE VARIABLES
+	public float speed = 5f;
+
+	// PRIVATE INSTANCE VARIABLE 
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		this._transform = gameObject.GetComponent<Transform> ();
+		 
+		this.Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this._currentPosition = this._transform.position; 
+		this._currentPosition -= new Vector2 (this.speed, 0);
+		this._transform.position = this._currentPosition; 
+
+		if (this._currentPosition.x <= -640) {
+			this.Reset ();
+		}
+	}
+
+	public void Reset ()
+	{
+		this._transform.position = new Vector2 (640f, 0);
+	}
+}
+
---
+++ Assets/_Scripts/PlaneController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed = 5f;
+
+	// PRIVATE INSTANCE VARIABLES
+	private float _playerInput;
+	private Transform _transform;
+	private Vector2 _currentPosition; 
+
+	// Use this for initialization
+	void Start () {
+		this._transform = gameObject.GetComponent<Transform> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this._currentPosition = this._transform.position;  
+
+		this._playerInput = Input.GetAxis ("Vertical"); 
+		// check if player input is negative and move down
+		if (this._playerInput < 0) {
+			this._currentPosition -= new Vector2 (0, this.speed);  
+
+		}
+		// check if player input is positive and move up 
+		if (this._playerInput > 0) {
+			this._currentPosition += new Vector2 (0, this.speed);  
+		}
+
+		this._checkBorders ();
+
+		this._transform.position = this._currentPosition;
+	}
+
+
+	// PRIVATE METHODS +++++
+
+	private void _checkBorders () {
+		// keep the plain inside the camera view
+		if(this._currentPosition.y < -208) {
+			this._currentPosition.y = -208;
+		}
+
+		if(this._currentPosition.y > 208) {
+			this._currentPosition.y =  208;
+		}
+
+	}
+}
