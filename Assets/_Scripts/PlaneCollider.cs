9eac79a539887938a0b680019a4ed785def48861
YaroslavKabanov <ya.kabanov@gmail.com>
2016-02-03 23:03:55 -0500

    Added audio, added collisions

---
+++ Assets/_Scripts/PlaneCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.CompareTag("Coin")) {
+			
+		}
+
+		if (other.gameObject.CompareTag ("Rocket")) {
+			Debug.Log ("lol");
+		}
+	}
+}
