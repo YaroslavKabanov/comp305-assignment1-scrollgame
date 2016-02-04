88f2cc7eb55509b2a38b9461b432ccc0590c728b
YaroslavKabanov <ya.kabanov@gmail.com>
2016-02-03 20:59:32 -0500

    Instantiate rocket object

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	//PUBLIC INSTANCE VARIABLES
+	public int rocketNumber = 2;
+	public RocketController rocket;
+
+	// Use this for initialization
+	void Start () {
+		this._initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Private methods +++++
+
+
+	// Initial methods
+	private void _initialize() {
+		for (int rocketCount = 0; rocketCount < this.rocketNumber; rocketCount++) {
+			Instantiate (rocket.gameObject);
+		}
+	}
+}
